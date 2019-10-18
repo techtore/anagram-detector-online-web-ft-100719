638463b9e6bbdb9fff3231a7b4c903f24406dac9
Victoria Hale <vic.hale74@gmail.com>
2019-10-18 00:11:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   def match(words)
     
     words.select do |word| 
-    (word.split(" ").sort) == (@word.split(" ").sort)
+    (word.split("").sort) == (@word.split("").sort)
   end
   end
 end
